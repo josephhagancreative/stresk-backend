a97fc3355676e1ddab4c9ccc07c246bfcb106cc6
Joe Hagan <jhaganfilm@gmail.com>
2023-02-26 18:49:18 +0900

    remade strapi structure

--- config/env/production/server.js
+++
@@ -1,7 +0,0 @@
-module.exports = ({ env }) => ({
-  url: env("https://stresk.herokuapp.com/"),
-  proxy: true,
-  app: {
-    keys: env.array("APP_KEYS", ["z40svj5fzhuwuvja", "r8d1bu6acqp1060l"]),
-  },
-})
